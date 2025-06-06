b1bd7880bc946f2d831a7dbb8ae83d0dc847b24b
Nicolet <jmlee337@gmail.com>
2025-06-05 23:19:41 -0500

    disable hotkeys
    fixes #3

--- src/slp2mp4/dolphin/ini.py
+++ src/slp2mp4/dolphin/ini.py
@@ -82,6 +82,17 @@ def make_gfx_file(userdir: pathlib.Path, user_settings):
         yield name
 
 @contextlib.contextmanager
+def make_hotkeys_file(userdir: pathlib.Path):
+    settings = {
+        "Hotkeys1": {
+            "Device": "/0/",
+        }
+    }
+    filename = userdir.joinpath("Config", "Hotkeys.ini")
+    with make_ini_file(filename, settings) as (name, handle):
+        yield name
+
+@contextlib.contextmanager
 def make_gecko_file(userdir: pathlib.Path):
     settings = {
         "Gecko": {},
--- src/slp2mp4/dolphin/runner.py
+++ src/slp2mp4/dolphin/runner.py
@@ -37,6 +37,7 @@ class DolphinRunner:
                 comm.make_temp_file(replay) as comm_file,
                 ini.make_dolphin_file(userdir) as dolphin_file,
                 ini.make_gfx_file(userdir, self.user_gfx) as gfx_file,
+                ini.make_hotkeys_file(userdir) as hotkeys_file,
                 ini.make_gecko_file(userdir) as gecko_file,
             ):
                 args = (
